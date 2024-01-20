9f02055aff984f19630b930006766eabde98be1f
cher lynn <97969453+cclshub@users.noreply.github.com>
2024-01-20 14:20:19 +0800

    fix: removed redundant code

--- frontend/src/components/CreateItineraryForm.jsx
+++ frontend/src/components/CreateItineraryForm.jsx
@@ -5,28 +5,22 @@ import TextInput from './TextInput.jsx'
 const CreateItineraryForm = ({ firstFieldRef, onCancel, onClick, props }) => {
         return (
             <Stack spacing={4}>
-                <FormControl>
                     <TextInput
                         label='Title'
                         id='title'
                         ref={firstFieldRef}
                         // defaultValue={props.title}
                     />
-                </FormControl>
-                <FormControl>
                     <TextInput 
                         label='Budget (SGD)' 
                         id='budget' 
                         // defaultValue={props.budget}
                     />
-                </FormControl>
-                <FormControl>
                     <TextInput 
                         label='Country' 
                         id='country' 
                         // defaultValue={props.country}
                     />
-                </FormControl>
                 <ButtonGroup display='flex' justifyContent='flex-end'>
                     <Button variant='outline' onClick={onCancel}>
                         Cancel
--- frontend/src/components/destinationComponents/PopoverForm.jsx
+++ frontend/src/components/destinationComponents/PopoverForm.jsx
@@ -3,40 +3,37 @@ import { Stack, Button, ButtonGroup, FormControl, FormLabel, Input } from '@chak
 import TextInput from '../TextInput.jsx'
 
 const PopoverForm = ({ firstFieldRef, onCancel, onClick, props }) => {
-        return (
-            <Stack spacing={4}>
-                <FormControl>
-                    <TextInput
-                        label='Destination Name'
-                        id='destination_name'
-                        ref={firstFieldRef}
-                        defaultValue={props.name}
-                    />
-                </FormControl>
-                <FormControl>
-                    <TextInput 
-                        label='Cost' 
-                        id='destination_cost' 
-                        defaultValue={props.cost}
-                    />
-                </FormControl>
-                <FormControl>
-                    <TextInput 
-                        label='Notes' 
-                        id='destination_notes' 
-                        defaultValue={props.notes}
-                    />
-                </FormControl>
-                <ButtonGroup display='flex' justifyContent='flex-end'>
-                    <Button variant='outline' onClick={onCancel}>
-                        Cancel
-                    </Button>
-                    <Button colorScheme='teal' onClick={onClick}>
-                        Save
-                    </Button>
-                </ButtonGroup>
-            </Stack>
-        )
-    }
+    const [destName, setDestName] = useState(props.name)
+    const [destCost, setDestCost] = useState(props.cost)
+    const [destNotes, setDestNotes] = useState(props.notes)
+    return (
+        <Stack spacing={4}>
+                <TextInput
+                    label='Destination Name'
+                    id='destination_name'
+                    ref={firstFieldRef}
+                    defaultValue={props.name}
+                />
+                <TextInput 
+                    label='Cost' 
+                    id='destination_cost' 
+                    defaultValue={props.cost}
+                />
+                <TextInput 
+                    label='Notes' 
+                    id='destination_notes' 
+                    defaultValue={props.notes}
+                />
+            <ButtonGroup display='flex' justifyContent='flex-end'>
+                <Button variant='outline' onClick={onCancel}>
+                    Cancel
+                </Button>
+                <Button colorScheme='teal' onClick={onClick}>
+                    Save
+                </Button>
+            </ButtonGroup>
+        </Stack>
+    )
+}
 
 export default PopoverForm
