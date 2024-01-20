70f45ee744bc8b3dd0f560a15840e61229c5127f
G1-Eric-Li <ericlitong@gmail.com>
2024-01-20 14:04:35 +0800

    chore: fix routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,36 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-
-import './App.css'
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
+import { PasswordField } from "./components/PasswordField";
+import { useState } from "react";
+import "./App.css";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import Login from "./pages/Login.jsx";
-import ShowItinerary from "./pages/ShowItinerary.jsx";
+import ShowItinerary from "./pages/showItinerary";
 import Dashboard from "./pages/dashboard";
-
-
+import Signup from "./pages/Signup.jsx";
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        {/* landing page */}
-        {/*<Route index element={<Navigate to="/login" />} />*/}
+	return (
+		<Router>
+			<Routes>
+				{/* landing page */}
+				<Route index element={<Navigate to="/login" />} />
 
-        <Route path="/login" element={<Login />} />
-				<Route path="/" element={<Dashboard />} />
-        <Route path="/showitinerary" element={<ShowItinerary />} />
-        {/*<Route path="/signup" element={<Signup />} />*/}
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
 
-
-        {/* protected, need to login first */}
-        {/*<Route element={<ProtectedRoute />}>*/}
-        {/*  <Route path="/homepage" element={<Homepage />} />*/}
-        {/*  <Route path="/create" element={<Create />} />*/}
-        {/*  <Route path="/read" element={<Read />} />*/}
-        {/*  <Route path="/update" element={<Update />} />*/}
-        {/*  <Route path="/delete" element={<Delete />} />*/}
-        {/*</Route>*/}
-      </Routes>
-    </Router>
-	)
-}
-
-
+				{/* protected, need to login first */}
+				<Route element={<ProtectedRoute />}>
+					<Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/showItinerary/:id" element={<ShowItinerary />} />
+				</Route>
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;
