08bdda29e3dd5e704867a6a55397619b93e860a7
G1-Eric-Li <ericlitong@gmail.com>
2024-01-20 11:12:16 +0800

    feat: made itinerary page

--- frontend/src/pages/showItinerary.jsx
+++ frontend/src/pages/showItinerary.jsx
@@ -2,17 +2,7 @@ import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Text, Container, Button } from "@chakra-ui/react";
 import axios from "axios";
-import {
-	Table,
-	Thead,
-	Tbody,
-	Tfoot,
-	Tr,
-	Th,
-	Td,
-	TableCaption,
-	TableContainer,
-} from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
 
 // const backendUrl = "http://localhost:9000";
@@ -22,78 +12,89 @@ const ShowItinerary = () => {
 	const navigate = useNavigate();
 	const queryParams = new URLSearchParams(location.search);
 	const id = queryParams.get("id");
+	const itinerary = {
+		destination1: { id: 1, name: "marina", cost: "10", notes: "lorem" },
+		destination2: { id: 2, name: "test", cost: "10", notes: "lorem" },
+	};
+	// const [itinerary, setitinerary] = useState("lorem ipsum");
 
-	const [itinerary, setitinerary] = useState("lorem ipsum");
+	// useEffect(() => {
+	// 	const fetchItinerary = async () => {
+	// 		try {
+	// 			const response = await axios
+	// 				.get
+	// 				// `${backendUrl}/transactions/${id}`
+	// 				();
 
-	useEffect(() => {
-		const fetchItinerary = async () => {
-			try {
-				const response = await axios
-					.get
-					// `${backendUrl}/transactions/${id}`
-					();
+	// 			// setitinerary(response.data);
+	// 		} catch (error) {
+	// 			console.error("Error fetching transaction:", error);
+	// 		}
+	// 	};
 
-				setitinerary(response.data);
-			} catch (error) {
-				console.error("Error fetching transaction:", error);
-			}
-		};
-
-		fetchItinerary();
-	}, [id]);
+	// 	fetchItinerary();
+	// }, [id]);
 
 	return (
 		<div>
 			{/* add header here*/}
-			<Container style={{ marginTop: "20px" }}>
-				<Text variant="h5" gutterBottom>
-					Itinerary Details
-				</Text>
+
+			<Container style={{ marginTop: "20px", textAlign: "left" }}>
+				<Heading>Itinerary Details</Heading>
 				{itinerary ? (
 					<>
 						<Text variant="body1">
 							<strong>ID:</strong> {itinerary.id}
 						</Text>
-						<Text variant="body1">
-							<strong>Description:</strong>{" "}
-							{itinerary.description}
-						</Text>
-						<Text variant="body1">
-							<strong>Amount:</strong> {itinerary.amount}
-						</Text>
+						<div
+							style={{
+								display: "flex",
+								justifyContent: "space-between",
+							}}>
+							<Heading size="md">Destinations</Heading>
+							<Button onClick={() => navigate("/homepage")}>
+								Back to Itinerary List
+							</Button>
+						</div>
 
-						<heading>Destinations</heading>
 						<Table variant="striped" colorScheme="teal">
-							<Tr>
-								<Th>Number</Th>
-								<Th>Name</Th>
-								<Th>Cost</Th>
-								<Th>Notes</Th>
-								<Th>Actions</Th>
-							</Tr>
-							<Tr>
-								<Td>1</Td>
-								<Td>marina</Td>
-								<Td>$10</Td>
-								<Td>lorem</Td>
-								<Button>Delete</Button>
-							</Tr>
-							<Tr>
-								<Td>2</Td>
-								<Td>test</Td>
-								<Td>$20</Td>
-								<Td>ipsun</Td>
-								<Button>Delete</Button>
-							</Tr>
+							<Thead>
+								<Tr>
+									<Th>Number</Th>
+									<Th>Name</Th>
+									<Th>Cost</Th>
+									<Th>Notes</Th>
+									<Th>Actions</Th>
+								</Tr>
+							</Thead>
+							<Tbody>
+								{Object.values(itinerary).map((destination) => (
+									<Tr key={destination.id}>
+										<Td>{destination.id}</Td>
+										<Td>{destination.name}</Td>
+										<Td>{destination.cost}</Td>
+										<Td>{destination.notes}</Td>
+										{/* both is popup/modal  */}
+										<Td>
+											<div
+												style={{
+													display: "flex",
+													justifyContent:
+														"space-between",
+												}}>
+												<Button style={{marginRight:'10px'}}>Update</Button>
+												<Button>Delete</Button>
+											</div>
+										</Td>
+									</Tr>
+								))}
+							</Tbody>
 						</Table>
 					</>
 				) : (
 					<Text variant="body1">Loading...</Text>
 				)}
 				<br />
-				<Button onClick={() => navigate("/homepage")}>
-					Back to Itinerary List
-				</Button>
 			</Container>
 		</div>
 	);
