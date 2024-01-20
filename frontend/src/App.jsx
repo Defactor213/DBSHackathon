bbce03db224cbca4a6834a089bf5878bcc894a1a
Zheng Teck <adamgoh1998@gmail.com>
2024-01-20 11:22:01 +0800

    chore: fix conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,28 +15,28 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import { PasswordField } from './components/PasswordField'
 import { useState } from 'react'
 import './App.css'
-import ProtectedRoute from "./components/protectedRoute";
+import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Login from "./pages/Login.jsx";
 
 
 const App = () => {
-
   return (
     <Router>
       <Routes>
         {/* landing page */}
         <Route index element={<Navigate to="/login" />} />
 
-        <Route path="/homepage" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        {/*<Route path="/signup" element={<Signup />} />*/}
 
         {/* protected, need to login first */}
-        <Route element={<ProtectedRoute />}>
-          <Route path="/create" element={<Create />} />
-          <Route path="/read" element={<Read />} />
-          <Route path="/update" element={<Update />} />
-          <Route path="/delete" element={<Delete />} />
-        </Route>
+        {/*<Route element={<ProtectedRoute />}>*/}
+        {/*  <Route path="/homepage" element={<Homepage />} />*/}
+        {/*  <Route path="/create" element={<Create />} />*/}
+        {/*  <Route path="/read" element={<Read />} />*/}
+        {/*  <Route path="/update" element={<Update />} />*/}
+        {/*  <Route path="/delete" element={<Delete />} />*/}
+        {/*</Route>*/}
       </Routes>
     </Router>)
 }
