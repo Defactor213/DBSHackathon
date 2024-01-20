c802a2e6d2ef5b2bca8faa70b78cfa3df6866a7e
Zheng Teck <adamgoh1998@gmail.com>
2024-01-20 10:01:34 +0800

    chore: remove frontend

--- frontend/src/components/protectedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Outlet, Navigate } from "react-router-dom";
-
-const ProtectedRoute = () => {
-	const username = localStorage.getItem("username");
-	return username ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
