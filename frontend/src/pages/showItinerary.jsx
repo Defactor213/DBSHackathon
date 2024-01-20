d19172b8adac09d5887c5286fac6ce6cb5a63553
cher lynn <97969453+cclshub@users.noreply.github.com>
2024-01-20 11:16:31 +0800

    chore: created template popover for edit destination

--- frontend/src/pages/showItinerary.jsx
+++ frontend/src/pages/showItinerary.jsx
@@ -1,65 +1,12 @@
-import { useState, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
-import { Text, Container, Button } from "@chakra-ui/react";
-import axios from "axios";
+import React from 'react'
+import DestinationModal from '../components/destinationComponents/DestinationPopover'
 
-// const backendUrl = "http://localhost:9000";
+const showItinerary = () => {
+  return (
+	<div>
+		<DestinationModal />
+	</div>
+  )
+}
 
-const ShowItinerary = () => {
-	const location = useLocation();
-	const navigate = useNavigate();
-	const queryParams = new URLSearchParams(location.search);
-	const id = queryParams.get("id");
-
-	const [transaction, setTransaction] = useState(null);
-
-	useEffect(() => {
-		const fetchTransaction = async () => {
-			try {
-				const response = await axios.get(
-					`${backendUrl}/transactions/${id}`
-				);
-
-				setTransaction(response.data);
-			} catch (error) {
-				console.error("Error fetching transaction:", error);
-			}
-		};
-
-		fetchTransaction();
-	}, [id]);
-
-	return (
-		<div>
-			<Container style={{ marginTop: "20px" }}>
-				<Text variant="h5" gutterBottom>
-					Transaction Details
-				</Text>
-				{transaction ? (
-					<>
-						<Text variant="body1">
-							<strong>ID:</strong> {transaction.id}
-						</Text>
-						<Text variant="body1">
-							<strong>Description:</strong>{" "}
-							{transaction.description}
-						</Text>
-						<Text variant="body1">
-							<strong>Amount:</strong> {transaction.amount}
-						</Text>
-					</>
-				) : (
-					<Text variant="body1">Loading...</Text>
-				)}
-				<Button
-					variant="outlined"
-					color="primary"
-					onClick={() => navigate("/homepage")}>
-					Back to Transactions List
-				</Button>
-			</Container>
-		</div>
-	);
-};
-
-export default ShowItinerary;
+export default showItinerary
