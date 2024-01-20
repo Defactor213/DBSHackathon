95b5ef028ac690494f39bff7e1ebeb6a6f6e1eec
yaohong6782 <yaohong6782@gmail.com>
2024-01-20 12:36:50 +0800

    frontend login fixed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,59 +6,68 @@ import {
   FormControl,
   FormLabel,
   Heading,
-  HStack, IconButton,
-  Input, InputGroup, InputRightElement,
-  Stack
+  HStack,
+  IconButton,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Stack,
 } from "@chakra-ui/react";
-import {useState} from "react";
-import {HiEye, HiEyeOff} from "react-icons/hi";
+import { useState } from "react";
+import { HiEye, HiEyeOff } from "react-icons/hi";
 import axios from "axios";
 
-
 export default function Login() {
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [isHidden, setisHidden] = useState(true)
+  const [isHidden, setisHidden] = useState(true);
 
-  const handleLogin = async () => {
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    console.log("submited");
+
     try {
-      const response = await axios.post('http://localhost:8000/user/userLogin', {
-        username: username,
-        lastname:password
-      });
-      console.log(response)
+      const response = await axios.post(
+        "http://localhost:8000/user/userLogin",
+        {
+          username: username,
+          password: password,
+        }
+      );
+      console.log(response);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
 
-  const setPasswordVisibility = () => !isHidden ? setisHidden(true) : setisHidden(false)
+  const setPasswordVisibility = () =>
+    !isHidden ? setisHidden(true) : setisHidden(false);
   return (
     <Container
       maxW="lg"
       py={{
-        base: '12',
-        md: '24',
+        base: "12",
+        md: "24",
       }}
       px={{
-        base: '0',
-        sm: '8',
+        base: "0",
+        sm: "8",
       }}
     >
       <Stack spacing="8">
         <Stack spacing="6">
           <Stack
             spacing={{
-              base: '2',
-              md: '3',
+              base: "2",
+              md: "3",
             }}
             textAlign="center"
           >
             <Heading
               size={{
-                base: 'xs',
-                md: 'sm',
+                base: "xs",
+                md: "sm",
               }}
             >
               Log in to your account
@@ -67,62 +76,77 @@ export default function Login() {
         </Stack>
         <Box
           py={{
-            base: '0',
-            sm: '8',
+            base: "0",
+            sm: "8",
           }}
           px={{
-            base: '4',
-            sm: '10',
+            base: "4",
+            sm: "10",
           }}
           bg={{
-            base: 'transparent',
-            sm: 'bg.surface',
+            base: "transparent",
+            sm: "bg.surface",
           }}
           boxShadow={{
-            base: 'none',
-            sm: 'md',
+            base: "none",
+            sm: "md",
           }}
           borderRadius={{
-            base: 'none',
-            sm: 'xl',
+            base: "none",
+            sm: "xl",
           }}
         >
           <Stack spacing="6">
             <Stack spacing="5">
-              <FormControl>
-                <FormLabel htmlFor="username">Username</FormLabel>
-                <Input id="username" type="text" value={username} onChange={e => setUsername(e.value)} />
-              </FormControl>
-              <FormControl>
-                <FormLabel htmlFor="password">Password</FormLabel>
-                <InputGroup>
-
-                  <InputRightElement>
-                    <IconButton
-                      variant="text"
-                      aria-label={isHidden ? 'Mask password' : 'Reveal password'}
-                      icon={isHidden ? <HiEye /> : <HiEyeOff />}
-                      onClick={setPasswordVisibility}
+              <form onSubmit={handleLogin}>
+                <FormControl>
+                  <FormLabel htmlFor="username">Username</FormLabel>
+                  <Input
+                    id="username"
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </FormControl>
+                <FormControl>
+                  <FormLabel htmlFor="password">Password</FormLabel>
+                  <InputGroup>
+                    <InputRightElement>
+                      <IconButton
+                        variant="text"
+                        aria-label={
+                          isHidden ? "Mask password" : "Reveal password"
+                        }
+                        icon={isHidden ? <HiEye /> : <HiEyeOff />}
+                        onClick={setPasswordVisibility}
+                      />
+                    </InputRightElement>
+                    <Input
+                      id="password"
+                      type={isHidden ? "password" : "text"}
+                      value={password}
+                      onChange={(e) => {
+                        setPassword(e.target.value);
+                      }}
                     />
-                  </InputRightElement>
-                <Input id="password" type={isHidden ? "password" : "text" } value={password} onChange={e => setPassword(e.value)} />
-                </InputGroup>
-              </FormControl>
-            </Stack>
-            <HStack justify="space-between">
-              <Button variant="text" size="sm" onClick={handleLogin}>
-                Forgot password?
-              </Button>
-            </HStack>
-            <Stack spacing="6">
-              <Button>Sign in</Button>
-              <HStack>
-                <Divider />
-              </HStack>
+                  </InputGroup>
+                </FormControl>
+                <HStack justify="space-between">
+                  <Button variant="text" size="sm">
+                    Forgot password?
+                  </Button>
+                </HStack>
+                <Stack spacing="6">
+                  <Button type="submit">Sign in</Button>
+                  <HStack>
+                    <Divider />
+                  </HStack>
+                </Stack>{" "}
+              </form>
             </Stack>
           </Stack>
         </Box>
       </Stack>
     </Container>
-  )
+  );
 }
