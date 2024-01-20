494b04b9b25e15ccec3d9acbb9128726e8da634a
yewjihao <yewjihao@gmail.com>
2024-01-20 14:22:43 +0800

    Country API

--- backend/routes/destinationRoutes.js
+++ backend/routes/destinationRoutes.js
@@ -8,5 +8,7 @@ router.get('/viewall',authenticationToken, destinationcontroller.viewDestination
 router.post('/create',authenticationToken, destinationcontroller.createDestination);
 router.delete('/delete/:destinationid',authenticationToken,destinationcontroller.deleteDestination);
 router.patch('/update',authenticationToken,destinationcontroller.updateDestination);
+router.get('/getallcountry',authenticationToken,destinationcontroller.getAllcountry); 
+router.get('/getcountry/:id',authenticationToken,destinationcontroller.getCountryName); 
 
 module.exports = router;
