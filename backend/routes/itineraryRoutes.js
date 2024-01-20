3b12439e8b2c91f7283a68638facd5b34c4a7cf3
nTynee <ntyneee@gmail.com>
2024-01-20 12:51:37 +0800

    Added post and patch request for itinerary

--- backend/routes/itineraryRoutes.js
+++ backend/routes/itineraryRoutes.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const itineraryController = require("../controllers/itinerary")
+const itineraryController = require("../controllers/itinerary");
+const { authenticationToken } = require("../authentication/authenticationToken")
 
 // all routes here are starting with /itinerary as initialised in app.js
 // ==== all itineraries ====
-router.get('/getItineraries', itineraryController.getItineraries);
+router.get('/getItineraries', authenticationToken, itineraryController.getItineraries);
 // ==== itineraries of a User ====
-router.get('/getItineraries/user/:id', itineraryController.getUserItineraries);
+router.get('/getItineraries/user/:id', authenticationToken, itineraryController.getUserItineraries);
 // ==== one itinerary ====
-router.get('/getItinerary/:id', itineraryController.getItinerary);
+router.get('/getItinerary/:id', authenticationToken, itineraryController.getItinerary);
 // ==== add itinerary ====
-router.post("/postItinerary", itineraryController.postItinerary);
+router.post("/postItinerary", authenticationToken, itineraryController.postItinerary);
+// ==== edit itinerary ====
+router.patch("/putItinerary/:id", authenticationToken, itineraryController.patchItinerary);
+
 
 module.exports = router;
