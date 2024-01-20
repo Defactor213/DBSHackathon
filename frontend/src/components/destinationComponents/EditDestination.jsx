83f544915a704528d0e71e0df0c15d7aab44a0ef
cher lynn <97969453+cclshub@users.noreply.github.com>
2024-01-20 14:45:34 +0800

    chore: setstates for create itinerary and edit destination forms

--- frontend/src/components/destinationComponents/EditDestination.jsx
+++ frontend/src/components/destinationComponents/EditDestination.jsx
@@ -14,13 +14,6 @@ import {useRef, useState} from "react";
 export default function EditDestination(props) {
   const { onOpen, onClose, isOpen } = useDisclosure()
   const firstFieldRef = useRef(null)
-  const [destName, setDestName] = useState(props.name)
-  const [destCost, setDestCost] = useState(props.cost)
-  const [destNotes, setDestNotes] = useState(props.notes)
-  const onClick = () => {
-    onClose()
-    return
-  }
   return (
     <div style={{textAlign: "center"}}>
       <Popover
@@ -41,7 +34,6 @@ export default function EditDestination(props) {
             <PopoverForm 
               firstFieldRef={firstFieldRef} 
               onCancel={onClose} 
-              onClick={onClick} 
               props={props}/>
           </FocusLock>
         </PopoverContent>
